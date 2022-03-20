64f515c5244a40c71c80502d83c719f42af89769
Olle Kelderman <omkelderman@gmail.com>
2022-03-20 16:57:21 +0100

    ignore direct replies to other ppl and imrpove discord webhook response logging

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ t.on('tweet', tweet => {
     console.log(`[${tweet.id_str}] tweet ${tweetUrl} received`);
     const discordWebhooks = CONFIG.hooksPerTwitterAccount[tweet.user.id_str];
     if (!discordWebhooks) {
-        console.log(`[${tweet.id_str}] this twitter account has no webhooks configured, do nothing`)
+        console.log(`[${tweet.id_str}] this twitter account has no webhooks configured, do nothing`);
+        return;
+    }
+
+    if(tweet.in_reply_to_user_id_str && tweet.in_reply_to_user_id_str != tweet.user.id_str) {
+        console.log(`[${tweet.id_str}] tweet is a reply to someone else, ignore tweet`)
         return;
     }
 
@@ -22,10 +27,17 @@ t.on('tweet', tweet => {
 
     console.log(`[${tweet.id_str}] this twitter account has ${discordWebhooks.length} webhook(s) configured, executing:`);
     for (const discordWebhook of discordWebhooks) {
-        console.log(`[${tweet.id_str}] shoot webhook ${discordWebhook.id}`);
+        console.log(`[${tweet.id_str}] [${discordWebhook.id}] shooting webhook...`);
         request.post({
             url: `https://discord.com/api/webhooks/${discordWebhook.id}/${discordWebhook.token}`,
             json: discordWebhookObject
+        }, (err, resp) => {
+            if(err) {
+                console.log(`[${tweet.id_str}] [${discordWebhook.id}] shooting webhook error`, err);
+                return;
+            }
+
+            console.log(`[${tweet.id_str}] [${discordWebhook.id}] webhook response http status code: ${resp.statusCode}`);
         });
     }
 });
